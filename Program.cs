0c0eb98a26f6b753a5470feebc74af58b5903c2a
Webster Boeing <webster.boeing@go.minneapolis.edu>
2025-03-06 21:00:11 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace VideoGames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<VideoGame> games = new List<VideoGame>();
+            games.Add(new VideoGame { Name = "Halo", Genre = "Shooter", Rating = "3.5" });
+            games.Add(new VideoGame { Name = "Super Mario", Genre = "Platformer", Rating = "4" });
+            games.Add(new VideoGame { Name = "Call of Duty", Genre = "Shooter", Rating = "3" });
+            games.Add(new VideoGame { Name = "Zelda", Genre = "Adventure", Rating = "5" });
+            games.Add(new VideoGame { Name = "Minecraft", Genre = "Sandbox", Rating = "2.5" });
+
+            // Query to get all games with a rating of 4 or higher 
+            var topRatedGames = (from game in games 
+                                where double.Parse(game.Rating) >= 3.5
+                                select game).ToList(); 
+
+            // Sort the games by Top Rated Games
+            var sortedGames = (from game in topRatedGames
+                                orderby game.Name
+                                select game).ToList();
+
+            // Print out the sorted games
+            foreach (var game in sortedGames)
+            {
+                Console.WriteLine(game.Name);}                    
+
+    }//end Main
+}}//end Program
---
+++ VideoGame.cs
@@ -0,0 +1,13 @@
+// Purpose: This file contains the VideoGame class which is used to store the attributes of a video game.
+
+namespace VideoGames
+{
+    public class VideoGame
+    {
+        //Attributes (Properties)
+        public required String Name { get; set; }
+        public required String Genre { get; set; }
+        public required String Rating { get; set; }
+    }
+
+}
